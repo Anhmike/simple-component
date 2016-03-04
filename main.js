fdc3693ff399b013bf2a60746a39ffca4eaf5d1b
danse <f.occhipinti@gmail.com>
2016-03-04 17:33:21 +0100

    it works within React now

--- main.js
+++ main.js
@@ -2,51 +2,54 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
 var d3 = require('d3')
-var axios = require('axios')
 var Select = require('react-select')
 require('bootstrap')
 
-function chart() {
-  // copied from http://bl.ocks.org/mbostock/3883245
+var LineChart = React.createClass({
+  componentDidMount: function () {
+    console.log('did mount')
+    var el = this.getDOMNode()
 
-  var margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+    // initially copied from http://bl.ocks.org/mbostock/3883245
 
-  var x = d3.time.scale()
-        .range([0, width]);
+    var margin = {top: 20, right: 20, bottom: 30, left: 50},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-  var y = d3.scale.linear()
-        .range([height, 0]);
+    var x = d3.time.scale()
+          .range([0, width]);
 
-  var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
+    var y = d3.scale.linear()
+          .range([height, 0]);
 
-  var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
+    var xAxis = d3.svg.axis()
+          .scale(x)
+          .orient("bottom");
 
-  var line = d3.svg.line()
-        .x(function(d) { return x(d.x); })
-        .y(function(d) { return y(d.y); });
+    var yAxis = d3.svg.axis()
+          .scale(y)
+          .orient("left");
 
-  var svg = d3.select("div#chart").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var line = d3.svg.line()
+          .x(function(d) { return x(d.x); })
+          .y(function(d) { return y(d.y); });
 
-  var groups = {}
+    var svg = d3.select(el).append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+          .append("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  groups.x = svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
+    var groups = {}
 
-  groups.y = svg.append("g")
-    .attr("class", "y axis")
+    groups.x = svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
 
-  groups.x
+    groups.y = svg.append("g")
+      .attr("class", "y axis")
+
+    groups.x
       .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
@@ -54,42 +57,66 @@ function chart() {
       .style("text-anchor", "end")
       .text("Price ($)");
 
-  var path = svg.append("path")
-        .attr("class", "line")
+    var path = svg.append("path")
+          .attr("class", "line")
 
-  function update(data) {
+    this.feedData = function (data) {
 
-    x.domain(d3.extent(data, function(d) { return d.x }))
-    y.domain(d3.extent(data, function(d) { return d.y }))
+      x.domain(d3.extent(data, function(d) { return d.x }))
+      y.domain(d3.extent(data, function(d) { return d.y }))
 
-    groups.x.call(xAxis);
-    groups.y.call(yAxis)
+      groups.x.call(xAxis);
+      groups.y.call(yAxis)
 
-    path
-      .datum(data)
-      .transition(2000)
-      .attr("d", line);
+      path
+        .datum(data)
+        .transition(2000)
+        .attr("d", line);
+
+    }
+  },
+  componentDidUpdate: function () {
+    console.log('did update')
+    this.feedData(this.props.data)
+  },
+  render: function () {
+    return (<div className="Chart"></div>)
   }
+})
 
-
-  return update
-}
-
-var options = [
-    { value: 'one', label: 'One' },
-    { value: 'two', label: 'Two' }
-];
+var SimpleComponent = React.createClass({
+  onChange: function (selection) {
+    this.setState({ label: selection.label })
+    component = this
+    d3.json(
+      "https://www.quandl.com/api/v3/datasets/"+selection.value+"/data.json",
+      function (data) {
+        logAll(data)
+        var stock = convertStock(data)
+        //logAll(stock)
+        //logAll(stock[0])
+        component.setState({ stock: stock })
+      }
+    )
+  },
+  getInitialState: function () {
+    return { stock: [] }
+  },
+  render: function () {
+    return (<div>
+      <Select.Async name="form-field-name"
+                    value="state.label"
+                    loadOptions={loadOptions}
+                    onChange={this.onChange} />
+      <LineChart data={this.state.stock} />
+      </div>)
+  }
+})
 
 ReactDOM.render(
-  <div>
-    <Select.Async name="form-field-name"
-                  loadOptions={loadOptions}
-                  onChange={fetchAndUpdate} />
-  </div>,
+  <SimpleComponent />,
   document.getElementById('select')
 );
-
-var update = chart()
 
 function convertStock (data) {
   function convert(d) {
@@ -102,19 +129,6 @@ function convertStock (data) {
   function getDate(row) { return row[0] }
   var points = data.dataset_data.data
   return points.map(convert)
-}
-
-function fetchAndUpdate (selection) {
-  d3.json(
-    "https://www.quandl.com/api/v3/datasets/"+selection.value+"/data.json",
-    function (data) {
-      logAll(data)
-      var points = convertStock(data)
-      logAll(points)
-      logAll(points[0])
-      update(points)
-    }
-  )
 }
 
 function loadOptions (input, callback) {
